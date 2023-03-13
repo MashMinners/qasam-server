a037ba10a6121f64fbfc5df4c266a6ed301e97a8
Rain <Valehtelit@mail.ru>
2023-03-13 21:31:24 +1000

    #Методы получения сотрудника/сотрудников, метод создания сотрудника

--- src/Application/ControlPanel/Controllers/EmployeeController.php
+++ src/Application/ControlPanel/Controllers/EmployeeController.php
@@ -4,22 +4,34 @@ declare(strict_types=1);
 
 namespace Application\ControlPanel\Controllers;
 
+use Application\Collector\Models\Employee;
 use Application\ControlPanel\Models\EmployeeManager;
+use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class EmployeeController
 {
-    public function __construct(EmployeeManager $manager){
+    public function __construct(private EmployeeManager $manager){
 
     }
 
-    public function show(ServerRequestInterface $request) : ResponseInterface {
-
+    public function get(ServerRequestInterface $request) : ResponseInterface {
+        $employeeId = file_get_contents('php://input');
+        $result = $this->manager->getEmployee($employeeId);
+        return new JsonResponse(($result));
     }
 
-    public function create(ServerRequestInterface $request) : ResponseInterface {
+    public function getAll(ServerRequestInterface $request) : ResponseInterface {
+        $result = $this->manager->getEmployees();
+        return new JsonResponse($result);
+    }
 
+    public function add(ServerRequestInterface $request) : ResponseInterface {
+        $json = file_get_contents('php://input');
+        $employeeId = $this->manager->insert(new Employee($json));
+        $response = (new JsonResponse($employeeId));
+        return $response;
     }
 
     public function edit(ServerRequestInterface $request) : ResponseInterface {
--- src/Application/ControlPanel/Models/EmployeeManager.php
+++ src/Application/ControlPanel/Models/EmployeeManager.php
@@ -4,7 +4,58 @@ declare(strict_types=1);
 
 namespace Application\ControlPanel\Models;
 
+use Application\Collector\Models\Employee;
+use Engine\Database\IConnector;
+use Ramsey\Uuid\Uuid;
+
 class EmployeeManager
 {
+    public function __construct(IConnector $connector) {
+        $this->pdo = $connector::connect();
+    }
+
+    public function getEmployee(string $id) : Employee {
+        $employeeId = json_decode($id)->employeeId;
+        $query = ("SELECT * FROM employees WHERE employee_id = :employeeId");
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(['employeeId' => $employeeId]);
+        $result = $stmt->fetch();
+        return new Employee($result);
+    }
+
+    public function getEmployees() : array {
+        $query = ("SELECT * FROM employees");
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+
+    //Находит дубликаты врачей
+    public function getEmployeeDuplicates(){
+
+    }
+
+    public function insert(Employee $employee) {
+        $query = ("INSERT INTO employees (employee_id, employee_surname, employee_first_name, employee_second_name, employee_position) 
+                    VALUES (:employeeId, :employeeSurname, :employeeFirstName, :employeeSecondName, :employeePosition)");
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([
+            'employeeId'=>$employeeId = Uuid::uuid4()->toString(),
+            'employeeSurname'=>$employee->employeeSurname,
+            'employeeFirstName'=>$employee->employeeFirstName,
+            'employeeSecondName'=>$employee->employeeSecondName,
+            'employeePosition'=>$employee->employeePosition
+        ]);
+        return $employeeId;
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
 
 }
